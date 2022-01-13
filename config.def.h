52a92d7febd08916f6bc365448e674650ae8852a
Christian LaForest <christianlaforest45@gmail.com>
2022-01-13 10:58:29 -0500

    added screenshot functionality with maim

--- config.def.h
+++ config.def.h
@@ -79,6 +79,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *screenshot[] = { "screenshot", NULL };
 
 static Key keys[] = {
 	/* modifier                     key           function        argument */
@@ -124,6 +125,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period,    focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,     tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,    tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_s,         spawn,          {.v = screenshot} },
 	TAGKEYS(                        XK_1,                         0)
 	TAGKEYS(                        XK_2,                         1)
 	TAGKEYS(                        XK_3,                         2)
